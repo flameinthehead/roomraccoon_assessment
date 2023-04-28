922e36d60155858e20a0b5578f78ead85f4b011f
Oleg Shmakov <o.shmakov@zaymigo.com>
2023-04-28 18:00:51 +0700

    Fix message.

--- index.php
+++ index.php
@@ -15,14 +15,14 @@ try {
     $router = new Router($_SERVER['REQUEST_METHOD'], $_SERVER['REQUEST_URI'], $container);
     $kernel = new Kernel($router);
     $kernel->run();
-} catch (\Exception $e) {
+} catch (Throwable $e) {
     $response = new Response(
         'error',
         Response::HTTP_BAD_REQUEST,
         [
             'message' => $e->getMessage(),
             'code' => Response::HTTP_BAD_REQUEST,
-            'data' => new \stdClass(),
+            'data' => ['message' => $e->getMessage()],
         ]
     );
     $response->render();
